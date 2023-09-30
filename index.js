5538a4b6df34f10b489f4b69e34680d94e852673
AbaadirYasin <abaadir.yasin@student.moringaschool.com>
2023-09-30 17:01:12 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+  
+    function handleClick() {
+      alert('Button was clicked!');
+    }
+  
+    button.addEventListener('click', handleClick);
+  }
+  
+  document.addEventListener('DOMContentLoaded', addingEventListener);
+  
